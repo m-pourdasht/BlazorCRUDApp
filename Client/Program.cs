0662205aaca3ea3d0e28d5c1879ad7e78f40b142
m.pourdasht <m.pourdasht@epsilonnet.gr>
2025-08-28 15:11:57 +0300

    healthy code

--- Client/Program.cs
+++ Client/Program.cs
@@ -11,6 +11,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp =>
     new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:7194/") 
+});
+
 builder.Services.AddScoped<IHttpService, HttpServiceClient>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
